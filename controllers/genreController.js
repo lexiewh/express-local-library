e982b1fc0cd37e05c844e5d728d9cf8b642348da
Lexie White <lexwhite028@gmail.com>
2020-12-16 16:06:37 -0600

    complete book detail view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,6 @@
 var Genre = require('../models/genre');
+var Book = require('../models/book')
+var async = require('async');
 
 // display list of all genres
 exports.genre_list = function(req, res) {
@@ -10,8 +12,26 @@ exports.genre_list = function(req, res) {
 };
 
 // display detail page for a specific genre
-exports.genre_detail = function(req, res) {
-    res.send('TODO: genre detail: ' + req.params.id)
+exports.genre_detail = function(req, res, next) {
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.params.id).exec(callback);
+        },
+
+        genre_books: function(callback) {
+            Book.find({ 'genre': req.params.id }).exec(callback);
+        },
+    }, function(err,  results) {
+        if (err) { return next(err); }
+        if (results.genre==null) {
+            // No results
+            var err = new Error('Genre not found');
+            err.status = 404;
+            return next(err);
+        }
+        // successful, so render
+        res.render('genre_detail', {title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books } );
+    });
 };
 
 // display genre create form on GET
